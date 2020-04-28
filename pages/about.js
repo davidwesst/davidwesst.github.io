2fd5796a54a8ca27991fb583d8a715a740d3a175
David Wesst <david@cocoboko.io>
2020-04-28 16:25:21 -0500

    WIP need to sort out date serialization error for posts

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,36 @@
 import Link from 'next/link'
 
-export default function About() {
+import { getSortedPostsData } from '../lib/blog'
+
+export default function About({ allPostsData }) {
     return (
         <>
             <h1>About DW!</h1>
             <Link href="/">
                 <a>Go Back Home</a>
             </Link>
+            <h2>Blog Posts</h2>
+            <ul>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li key={id}>
+                    {title}
+                    <br />
+                    {id}
+                    <br />
+                    {date}
+                    </li>
+                ))}
+            </ul>
+
         </>
     )
 }
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+      props: {
+        allPostsData
+      }
+    }
+  }
