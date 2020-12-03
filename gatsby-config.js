acd40fff0b4b98c6ade778462d54a0f539644b3a
David Wesst <david@cocobokostudios.com>
2020-12-03 10:13:52 -0600

    generating a blog/rss.xml file. resolves #5

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@
 module.exports = {
   siteMetadata: {
     title: `davidwesst.com`,
+    siteUrl: `https://www.davidwesst.com`,
+    description: `Amateur GameDev Done Professionally`,
     tagline: `Amateur GameDev Done Professionally`,
     author: {
       name: `David Wesst`,
@@ -32,6 +34,47 @@ module.exports = {
         path: `${__dirname}/content/blog`
       }
     },
-    `gatsby-transformer-remark`
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: `${site.siteMetadata.siteUrl}/blog${edge.node.fields.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}/blog${edge.node.fields.slug}`,
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/blog/rss.xml",
+            match: "^/blog/"
+          }
+        ]
+      }
+    }
   ],
 }
