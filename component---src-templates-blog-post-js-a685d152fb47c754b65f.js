54890da30fc22b473bfa1c7c80ff47fc9c42b7e2
davidwesst <davidwesst@users.noreply.github.com>
2021-02-10 22:03:38 +0000

    added favicon and added to head of base layout. resolves #8 20e3078d9af8bffe99e1ec7f930899b5f99f4f63

---
+++ component---src-templates-blog-post-js-a685d152fb47c754b65f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{yZlL:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return u}));var a=n("q1tI"),r=n.n(a),l=n("Bl7J");function u(e){var t=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("h1",null,t.frontmatter.title),r.a.createElement("article",{dangerouslySetInnerHTML:{__html:t.html}}))}}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-a685d152fb47c754b65f.js.map
