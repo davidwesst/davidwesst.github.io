918df4a61bb4e7b8da28533b2fbb1c71085dc1c6
David Wesst <dw@davidwesst.com>
2021-07-06 23:52:51 -0500

    fixed process env variable name

--- src/components/comments.js
+++ src/components/comments.js
@@ -7,7 +7,7 @@ const Comments = ({ slug }) => {
     {`   /* - - - CONFIGURATION VARIABLES - - - */
 
         var __semio__params = {
-        graphcommentId: "${process.env.GRAPHCOMMENTS_ID}", // make sure the id is yours
+        graphcommentId: "${process.env.GRAPHCOMMENT_ID}", // make sure the id is yours
 
         behaviour: {
             // HIGHLY RECOMMENDED
