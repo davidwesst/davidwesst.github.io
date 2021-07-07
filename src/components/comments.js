4acc8a4862071da272aba8dcfb89f05b0423ed1b
David Wesst <dw@davidwesst.com>
2021-07-06 23:45:37 -0500

    added comment component. version 7.1.0-beta

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,12 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import Script from 'react-inline-script';
 
 const Comments = ({ slug }) => {
     return (
-        <div>
-            <h5>Comments go here</h5>
-            <p>{slug}</p>
-        </div>
-    )
+    <Script>
+    {`   /* - - - CONFIGURATION VARIABLES - - - */
+
+        var __semio__params = {
+        graphcommentId: "${process.env.GRAPHCOMMENTS_ID}", // make sure the id is yours
+
+        behaviour: {
+            // HIGHLY RECOMMENDED
+            uid: "${slug}", // uniq identifer for the comments thread on your page (ex: your page id)
+        },
+
+        // configure your variables here
+
+        }
+
+        /* - - - DON'T EDIT BELOW THIS LINE - - - */
+
+        function __semio__onload() {
+        __semio__gc_graphlogin(__semio__params)
+        }
+
+
+        (function() {
+        var gc = document.createElement('script'); gc.type = 'text/javascript'; gc.async = true;
+        gc.onload = __semio__onload; gc.defer = true; gc.src = 'https://integration.graphcomment.com/gc_graphlogin.js?' + Date.now();
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(gc);
+        })();
+    `}
+    </Script>
+    );
 };
 
 export default Comments;
