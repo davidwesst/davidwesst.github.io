e5c3c683ee3633c81f1f32e8aa49ec385b48d89a
David Wesst <david@cocobokostudios.com>
2021-11-15 22:39:12 +0000

    moved global and layout component styles to css modules. #67

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,39 +3,22 @@ import Seo from './seo';
 import Header from './header';
 import Footer from './footer';
 import Container from './container';
-import GlobalStyle from './global-styles';
-import styled from 'styled-components';
+
+import '../styles/global.css';
 
 const Layout = ({ children, title, description, socialImage = '' }) => {
-  console.log(socialImage);
   return (
     <Fragment>
-      <GlobalStyle />
       <Seo title={title} description={description} image={socialImage} />
-      <LayoutWrapper>
+      <>
         <Header />
         <main>
           <Container>{children}</Container>
         </main>
         <Footer />
-      </LayoutWrapper>
+      </>
     </Fragment>
   );
 };
 
 export default Layout;
-
-const LayoutWrapper = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-
-  & main {
-    margin-top: auto;
-    margin-bottom: auto;
-  }
-
-  & footer {
-    margin-top: auto;
-  }
-`;
