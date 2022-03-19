d9aede9b24543f509a680ceedd11cbd6eb6f117e
David Wesst <david@cocobokostudios.com>
2022-03-19 18:01:26 +0000

    updated layout of project list component

--- src/components/project-list.tsx
+++ src/components/project-list.tsx
@@ -2,19 +2,26 @@ import { useStaticQuery, graphql } from "gatsby";
 import { getImage, GatsbyImage } from "gatsby-plugin-image";
 import React from "react";
 
-import ProjectListItem, { IProjectItemProps } from './project-list-item';
-
-interface ProjectListProps {
-    projectData: Array<IProjectItemProps>;
-}
+import * as styles from '../styles/project-list.module.css';
 
 const RenderLink = (hrefValue: string) => {
     if(hrefValue) {
-        return <a href={hrefValue}>Learn More</a>
+        return <a href={hrefValue}>Check it out!</a>
     }
     else {
         return <div>More Info Soon...</div>
     }
+}
+
+const RenderProjectImage = (thumbnailImage, thumbnailAlt) => {
+    const projectImg = getImage(thumbnailImage);
+    return (
+        <GatsbyImage 
+            image={projectImg} 
+            alt={thumbnailAlt}
+            className={styles.projectImage}
+            />
+    );
 }
 
 const ProjectList : React.FunctionComponent = ( () => {
@@ -27,7 +34,10 @@ const ProjectList : React.FunctionComponent = ( () => {
                         link
                         thumbnail {
                             childImageSharp {
-                                ...fluid
+                                gatsbyImageData(
+                                    width: 256
+                                    placeholder: BLURRED
+                                )
                             }
                         }
                         thumbnailAlt
@@ -42,12 +52,17 @@ const ProjectList : React.FunctionComponent = ( () => {
     return (
         <>
         {data.allProjectsYaml.nodes.map((projectItem) => (
-            <article key={projectItem.id} >
-                <h3>{projectItem.title}</h3>
-                <p>{projectItem.description}</p>
-                <img src={projectItem.thumbnail.publicURL} alt={projectItem.thumbnailAlt} />
-                {RenderLink(projectItem.link)}
-            </article>
+            <>
+            <article key={projectItem.id} className={styles.project} >
+                {RenderProjectImage(projectItem.thumbnail, projectItem.thumbnailAlt)}
+                <div className={styles.projectInfo}>
+                    <h3>{projectItem.name} ({projectItem.type})</h3>
+                    <p>{projectItem.description}</p>
+                    {RenderLink(projectItem.link)}
+                </div>
+            </article>   
+            <hr key={`${projectItem.id}_spacer`} />
+            </>
         ))}
         </>
     )
