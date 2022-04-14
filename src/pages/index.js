68abc5d72359d0573e751909f0590a5d45312613
David Wesst <david@cocobokostudios.com>
2022-04-14 02:19:42 +0000

    updated splash page message

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,10 @@ const Index = ({ data }) => {
             <section className={style.splash}>
                 <article>
                   <p>Hey friendo, it's DW.</p>
-                  <p>I play and build cool stuff on the internet.</p>
+                  <p>I build cool stuff with code.</p>
                 </article>
                 <article>
-                  <p>I <a href="https://twitch.tv/davidwesst">livestream at 9pm (UTC-6)</a> on Twitch every Tuesday (Play) and Thursday (Code). See the <a href="https://twitch.tv/davidwesst/schedule"> schedule</a> for details and changes, just in case.</p>
+                  <p>I <a href="https://twitch.tv/davidwesst">livestream code on Twitch</a>. See the <a href="https://twitch.tv/davidwesst/schedule"> schedule</a> for details.</p>
                   <p>I tend to share non-live content about tech stuff I like and do on <a href="https://youtube.com/davidwesst">YouTube</a> and on my <Link to="/blog">Blog</Link>.</p>
                   <p>Oh, and sometimes I like to throw random thoughts, links, and updates on <a href="https://twitter.com/davidwesst">Twitter</a>.</p>
                 </article>
