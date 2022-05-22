d0d002cc3ed90e6eb9015e9c82082afddf21951d
David Wesst <david@cocobokostudios.com>
2022-05-22 05:45:51 +0000

    added shortcodes for legacy blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -12,8 +12,29 @@ module.exports = (eleventyConfig) => {
         return new CleanCSS({}).minify(code).styles;
     });
 
+    // Shortcodes
+    eleventyConfig.addShortcode("asset_img", (imgFile)=> {
+        return `<span>image goes here</span>`
+    });
+    eleventyConfig.addShortcode("img", (imgFile)=> {
+        return `<span>image goes here</span>`
+    });
+    eleventyConfig.addShortcode("youtube", (ytCode)=> {
+        return `<span>youtube video goes here</span>`
+    });
+    eleventyConfig.addShortcode("codeblock", (ytCode)=> {
+        return `<span>codeblock video goes here</span>`
+    });
+    eleventyConfig.addShortcode("endcodeblock", (ytCode)=> {
+        return `<span>endcodeblock video goes here</span>`
+    });
+    eleventyConfig.addShortcode("post_link", (ytCode)=> {
+        return `<span>post_link goes here</span>`
+    });
+
     // Alias
     eleventyConfig.addLayoutAlias("base", "base.njk");
+    eleventyConfig.addLayoutAlias("post", "post.njk");
     
     return {
         dir: {
