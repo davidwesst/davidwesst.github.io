a2aca82efdf8730b9f636cd68c03699d1ab49262
David Wesst <david@cocobokostudios.com>
2022-09-22 17:49:34 +0000

    generating pages for talks from JSON data

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
-  eleventyConfig.addPassthroughCopy("./src/assets")
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/blog/**/*.{jpg,jpeg,png,gif}");
 
   return {
     dir: {
         input: "src",
-        output: "dist"
+        output: "dist",
+        layouts: "_layouts"
     },
     passthroughFileCopy: true
   }
