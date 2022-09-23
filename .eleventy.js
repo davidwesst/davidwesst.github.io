921c1090508c808e94671019b381d19925d007be
David Wesst <david@cocobokostudios.com>
2022-09-23 02:28:36 +0000

    added site header component

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/blog/**/*.{jpg,jpeg,png,gif}");
