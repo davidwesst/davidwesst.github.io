84b219fed73a132d31448d07d2f01d7d044aa0a9
David Wesst <david@cocobokostudios.com>
2022-10-10 15:02:45 -0500

    removed duplicate require

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 const markdownIt = require("markdown-it");
 const markdownItEmoji = require("markdown-it-emoji");
-//const markdownItImages = require("markdown-it-eleventy-img");
 const markdownItImages = require("markdown-it-eleventy-img");
 
 const path = require("path");
