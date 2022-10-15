d7c0141e89c67452630650b13e034ce6126fd0f9
David Wesst <david@cocobokostudios.com>
2022-10-15 01:31:57 -0500

    setup application insights and environment variables

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -1,1 +1,9 @@
-console.log("Typescript is here now!");
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+
+// setup application insights
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: process.env.AI_CONNECTION_STRING
+}});
+
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
