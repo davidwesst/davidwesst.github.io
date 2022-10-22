b9aa2d44ab3af9499ffa06d97f7a94047c4f5fd3
David Wesst <david@cocobokostudios.com>
2022-10-22 01:23:04 -0500

    first pass at gdpr dialogue and script. not working correctly yet. #141

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -6,8 +6,35 @@ const appInsights = new ApplicationInsights({ config: {
   enableCorsCorrelation: true,
   enableRequestHeaderTracking: true,
   enableResponseHeaderTracking: true,
-  enableUnhandledPromiseRejectionTracking: true
+  enableUnhandledPromiseRejectionTracking: true,
+  disableCookiesUsage: true
 }});
 
 appInsights.loadAppInsights();
 appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
+
+const acceptCookies = (accepted: boolean) => {
+	const gdprEventBody = {
+		name: (accepted) ? "gdpr_accept" : "gdpr_reject"
+	};
+
+	if(accepted) {
+		// enable cookies
+		appInsights.getCookieMgr().setEnabled(true);
+	}
+
+	// save result to local storage
+	window.localStorage.setItem("gdprResult", accepted.toString());
+
+	// track response
+	appInsights.trackEvent(gdprEventBody);
+}
+
+const btnGdprAccept = document.getElementById("btnGdprAccept");
+btnGdprAccept.addEventListener("click", ()=> {
+	acceptCookies(true);
+});
+const btnGdprReject = document.getElementById("btnGdprReject");
+btnGdprReject.addEventListener("click", ()=> {
+	acceptCookies(false);
+});
