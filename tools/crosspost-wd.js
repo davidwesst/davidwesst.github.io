75541eaa7d15eb4a06f1aa02459b971081e26148
David Wesst <david@cocobokostudios.com>
2022-11-16 23:55:03 -0600

    setup scaffold for crosspost script for westerndevs. #36

---
+++ tools/crosspost-wd.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const git = require("simple-git");
+const path = require("path");
+const os = require("os");
+
+// setup temp working directory
+const working_dir_path = path.join(os.tmpdir(), "crosspost-wd", "_");
+const working_dir = fs.mkdtempSync(
+    path.join(os.tmpdir(),"crosspost-wd_") 
+);
+
+// clean-up temp directory
+fs.rmdirSync(working_dir);
+
+
