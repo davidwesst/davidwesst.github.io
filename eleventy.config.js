d4c84cba2337ca1328de11f988a6173061b46888
David Wesst <david@cocobokostudios.com>
2024-12-06 20:06:19 -0600

    feature. Added core site pages and site header component.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,5 @@
+import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
@@ -6,6 +8,12 @@ export default function (eleventyConfig) {
     eleventyConfig.setOutputDirectory("_site");
     eleventyConfig.setDataDirectory("../data");
     eleventyConfig.setLayoutsDirectory("../layouts");
+
+    /*
+        Plugins
+    */
+    
+    eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
     eleventyConfig.addPlugin(pluginWebc, {
         components: [
