a5ff85e60c2c37389b0a16881f3bf95e01d52c94
David Wesst <david@cocobokostudios.com>
2024-12-07 00:57:03 -0600

    feature. Added atom feed.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
 
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
@@ -14,6 +15,25 @@ export default function (eleventyConfig) {
     */
     
     eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "atom",
+        outputPath: "/feed.xml",
+        collection: {
+            name: "blog",
+            limit: 10    
+        },
+        metadata: {
+            language: "en",
+            title: "Blog by David Wesst",
+            subtitle: "A blog about things I enjoy, namely video games, software development, and video game software development",
+            base: "https://davidwesst.com/",
+            author: {
+                name: "David Wesst",
+                email: ""
+            }
+        }
+    });
 
     eleventyConfig.addPlugin(pluginWebc, {
         components: [
