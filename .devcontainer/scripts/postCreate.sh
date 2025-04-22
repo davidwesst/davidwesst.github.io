be00cf71123872760d8c4903cf82b9636b553e69
David Wesst <david@cocobokostudios.com>
2025-04-22 02:50:13 +0000

    feature. Added postCreate shell script and used in the devcontainer to add extra configuration to solve the freezing issue.

---
+++ .devcontainer/scripts/postCreate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Update NPM
+echo "Updating npm..."
+npm install -g npm@latest
+
+# Install project dependencies
+echo "Installing npm dependencies..."
+npm install
+
+# Install Playwright Dependencies
+echo "Installing Playwright dependencies..."
+npx playwright install --with-deps
+
+# Setup and checkout files with Git LFS
+echo "Checking out LFS files..."
+git lfs install
+git lfs pull
+git lfs checkout
+
+# Build the project
+echo "Building the project..."
+npm run build
+
+# Print completion message
+echo "Post-create commands executed successfully!"
